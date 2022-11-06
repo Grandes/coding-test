5fc24bfb477066eae67b7f7afb42e14c3f03e739
권용호 <kyh5270@gmail.com>
2022-11-06 17:28:53 +0900

    feat(프로그래머스 코딩테스트 Lv0 - 옹알이) #1 - Java 해답

---
+++ programmers/algorithm/java/src/Level0_옹알이.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class Level0_옹알이 {
+    private static int answer;
+
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        String[] babbling = {"aya", "yee", "u", "maa", "wyeoo"};
+//        String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+
+        System.out.println("result : " + solution(babbling));
+    }
+
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        String[] check = {"aya", "ye", "woo", "ma"};
+        for (String babblingString : babbling) {
+            for (String checkString : check) {
+                if (babblingString.contains(checkString)) {
+                    babblingString = babblingString.replace(checkString, ".");
+                }
+                if(babblingString.length() == babblingString.length() - babblingString.replace(".", "").length()) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(babbling));
+
+        return answer;
+    }
+}
