8cdc56dc5df267ec11c46e9b087122d9b6199ac6
권용호 <kyh5270@gmail.com>
2022-11-20 14:42:00 +0900

    feat(프로그래머스 코딩테스트 Lv0) #1 - 다음에 올 숫자 / 연속된 수의 합 - Java 해답

---
+++ programmers/algorithm/java/src/Level0_다음에올숫자.java
@@ -0,0 +1,27 @@
+public class Level0_다음에올숫자 {
+
+    public static void main(String[] args) {
+
+//        int[] common = {1, 2, 3, 4};  // result : 5
+        int[] common = {2, 4, 8};  // result : 16
+
+        // 등차 혹은 등비수열...
+        int result = solution(common);
+
+        System.out.println("result : " + result);
+    }
+
+    public static int solution(int[] common) {
+        int answer = 0;
+
+        if(common[2] - common[1] == common[1] - common[0]) {
+            // 등차수열
+            answer = common[common.length -1] + common[1] - common[0];
+        } else {
+            // 등비수열
+            answer = common[common.length -1] * common[1] / common[0];
+        }
+
+        return answer;
+    }
+}
---
+++ programmers/algorithm/java/src/Level0_연속된수의합.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class Level0_연속된수의합 {
+    public static void main(String[] args) {
+        int num = 4;
+        int total = 14;
+
+//        num total result
+//        3	12	[3, 4, 5]
+//        5	15	[1, 2, 3, 4, 5]
+//        4	14	[2, 3, 4, 5]
+//        5	5	[-1, 0, 1, 2, 3]
+
+        int[] result = solution(num, total);
+        System.out.println("resut : " + Arrays.toString(result));
+    }
+
+    public static int[] solution(int num, int total) {
+        int[] answer = new int[num];
+
+        int arrayMiddle = total / num;
+        if((total % num) == 0) {
+            for(int i = 0; i < num; i++) {
+                answer[i] = arrayMiddle - (num / 2) + i;
+            }
+        } else {
+            for(int i = 0; i < num; i++) {
+                answer[i] = arrayMiddle - (num / 2) + i + 1;
+            }
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+
+    public static int[] bestSolution(int num, int total) {
+        int[] answer = new int[num];
+        int check = num*(num+1) / 2;
+        int start = (total - check) / num + 1;
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start + i ;
+        }
+        return answer;
+    }
+}
